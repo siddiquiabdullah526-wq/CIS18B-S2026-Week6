06a69ffb43cc34cf676aaec54c552e3ec154ff74
siddiquiabdullah526-wq <siddiquiabdullah526@gmail.com>
2026-04-14 04:03:27 +0000

    Completed Mini 4: Type Erasure Lab with extension and comments

--- week6-exercises/mini04-type-erasure-lab/src/main/java/edu/norcocollege/cis18b/week6/mini04/TypeErasureLab.java
+++ week6-exercises/mini04-type-erasure-lab/src/main/java/edu/norcocollege/cis18b/week6/mini04/TypeErasureLab.java
@@ -12,6 +12,8 @@ public class TypeErasureLab {
         boolean sameRuntimeClass = courseNames.getClass() == sectionCounts.getClass();
         boolean listInstanceCheck = courseNames instanceof List;
 
+        // Type erasure removes generic type arguments at runtime, so both lists share the same runtime class
+        // illegal example avoided: you can't write "courseNames instanceof List<String>".
         System.out.println("Same runtime class: " + sameRuntimeClass);
         System.out.println("List is instance of java.util.List: " + listInstanceCheck);
         System.out.println("Course token simple name: " + TypeTokenPrinter.simpleNameOf(String.class));
--- week6-exercises/mini04-type-erasure-lab/src/main/java/edu/norcocollege/cis18b/week6/mini04/TypeTokenPrinter.java
+++ week6-exercises/mini04-type-erasure-lab/src/main/java/edu/norcocollege/cis18b/week6/mini04/TypeTokenPrinter.java
@@ -5,6 +5,9 @@ public final class TypeTokenPrinter {
     private TypeTokenPrinter() {
     }
 
+    // this is implementation of the extension challenge
+    // Because of type erasure, API logging and diagnostics usually cannot see generic type arguments at runtime
+    // unless a Class<T> token or similar metadata is passed explicitly.
     public static <T> String simpleNameOf(Class<T> token) {
         return token.getSimpleName();
     }
